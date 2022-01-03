e0c054ebfd59676fc32c08d9611887a4cfb6ac44
tarun0691 <51128120+tarun0691@users.noreply.github.com>
2022-01-02 16:56:51 -0800

    Update Client.java

--- spring-loose-coupling/src/main/java/com/practice/spring/lc/Client.java
+++ spring-loose-coupling/src/main/java/com/practice/spring/lc/Client.java
@@ -9,7 +9,7 @@ public class Client {
         //IOC container that reads the beans xml file and gets all the objects from the container
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        //this will get the bean named englishStudent from beans.xml and inject all the dependencies(literal and objective)
+        //this will get the bean named student from beans.xml and inject all the dependencies(literal and objective)
         Student student = context.getBean("student", Student.class);
         student.cheating();
     }
